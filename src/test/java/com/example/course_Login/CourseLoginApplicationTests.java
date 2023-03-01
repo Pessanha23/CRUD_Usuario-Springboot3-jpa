e9036ad0497c7c373b0e746b689a2fedf8940c63
João Ricardo Pagano Pessanha <joao.ricardopagano@gmail.com>
2023-03-01 15:16:37 -0300

    Adicionado dependencia JUnit, e iniciação dos testes unitários do CRUD, no SpringBoot.

--- src/test/java/com/example/course_Login/CourseLoginApplicationTests.java
+++ src/test/java/com/example/course_Login/CourseLoginApplicationTests.java
@@ -1,13 +1,34 @@
 package com.example.course_Login;
 
-import org.junit.jupiter.api.Test;
+import com.example.course_Login.entities.Usuario;
+import com.example.course_Login.service.UsuarioService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class CourseLoginApplicationTests {
+
+	@Autowired
+	UsuarioService usuarioService;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void teste_1(){
+		UsuarioService usuarioService = new UsuarioService();
+		List<Usuario> todosUsuarios = usuarioService.findAll();
+
+		String expectativa =
+
+		Assert.assertEquals(, todosUsuarios);
+
+
+	}
 }
